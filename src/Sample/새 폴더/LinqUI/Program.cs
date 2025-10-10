a84492059e853a1f9ebd2c9cd5a7611ad87e9113
lukewire <lukewire129@gmail.com>
2025-10-10 23:25:16 +0900

    LinqUI 샘플 추가

---
+++ src/Sample/새 폴더/LinqUI/Program.cs
@@ -0,0 +1,40 @@
+﻿using LinqUI.WPF;
+using LinqUI.WPF.HotReload;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+HotReloadManager.Enable ();
+
+var builder = Host.CreateApplicationBuilder ();
+
+var app = builder.BuildApp ();  // 🔥
+
+app.Run ();
+
+
+public class App : Application { }
+public class MainWindow : Window, IRender
+{
+    public MainWindow()
+    {
+        this.Render ();
+    }
+    public void Render()
+    {
+        this.Content = new Grid ()
+                            .Columns ("*, *, *")
+                            .Children(
+                                new Button()
+                                    .Content("핫리로드!")
+                                    .Foreground(Colors.White)
+                                    .Background(Colors.Green),
+                                new Button ()
+                                    .Content ("둘째!!")
+                                    .Column(1),
+                                new Button ()
+                                    .Content ("셋째!")
+                                    .Column (2)
+                            );
+    }
+}
