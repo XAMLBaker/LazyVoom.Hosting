d53d8819623ac4fcad05e2c1158fe63612559798
lukewire <lukewire129@gmail.com>
2025-10-10 23:37:14 +0900

    LazyVoom 라이브러리 연동

---
+++ LazyVoom.ServiceProvider/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace LazyVoom.ServiceProvider;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty] int count;
+
+    [RelayCommand]
+    private void Increment()
+    {
+        Count++;
+    }
+
+    [RelayCommand]
+    private void Decrement()
+    {
+        Count--;
+    }
+}
---
+++ LazyVoom.ServiceProvider/Program.cs
@@ -0,0 +1,17 @@
+﻿using LazyVoom.Core;
+using LazyVoom.ServiceProvider;
+
+var builder = Host.CreateApplicationBuilder ();
+
+builder.Services.AddSingleton<MainWindowViewModel> ();
+var app = builder.BuildApp ();  // 🔥
+
+app.OnStartUpAsync = async provider =>
+{
+    Voom.Instance.WithContainerResolver (vmType =>
+    {
+        return provider.GetService (vmType) ?? ActivatorUtilities.CreateInstance (provider, vmType);
+    });
+};
+
+app.Run ();
