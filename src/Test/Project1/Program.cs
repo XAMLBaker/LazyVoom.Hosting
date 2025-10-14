28230bbaea1003c35664320ecfcf1ce02dd1c446
lukewire <lukewire129@gmail.com>
2025-10-14 09:16:48 +0900

    테스트 프로젝트 추가

---
+++ src/Test/Project1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Project1;
+
+var builder = Host.CreateApplicationBuilder ();
+
+var app = builder.BuildApp<App, MainWindow> ();  // 🔥
+
+app.OnStartUpAsync = async provider =>
+{
+
+};
+// Exit 시 정리
+app.OnExitAsync = async provider =>
+{
+    Console.WriteLine ("앱 종료 중...");
+    await Task.Delay (200);
+};
+
+app.Run ();
